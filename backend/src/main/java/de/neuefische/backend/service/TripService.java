fd306e12cfdec032a691a550258028e0c1625bff
Florian Knoerzer <knoerzerflorian@web.de>
2022-05-19 14:17:01 +0200

    Add Service

---
+++ backend/src/main/java/de/neuefische/backend/service/TripService.java
@@ -0,0 +1,29 @@
+package de.neuefische.backend.service;
+
+import de.neuefische.backend.model.Trip;
+import de.neuefische.backend.repository.TripRepo;
+import lombok.Builder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Builder
+public class TripService {
+
+    private final TripRepo tripRepo;
+
+    @Autowired
+    public TripService(TripRepo tripRepo) {
+        this.tripRepo = tripRepo;
+    }
+
+    public List<Trip> getTrips() {
+        return tripRepo.findAll();
+    }
+
+    public Trip addNewTrip(Trip trip) {
+       return tripRepo.insert(trip);
+    }
+}
