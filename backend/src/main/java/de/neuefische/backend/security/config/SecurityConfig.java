f7726cc0791cc9d2f768d6b32e82802a6bf0832e
Florian Knoerzer <knoerzerflorian@web.de>
2022-06-10 12:07:32 +0200

    SecurityConfig added

---
+++ backend/src/main/java/de/neuefische/backend/security/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package de.neuefische.backend.security.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+}
